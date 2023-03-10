703ce4ca412f5fa0aec7ecd3ee6cd620324bea2e
chuwiii <chuchenwei7@gmail.com>
2023-03-10 12:52:22 +0800

    added firebase

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,44 +1,21 @@
 import Card from "../UI/Card";
 import MealItem from "./MealItem/MealItem";
 import classes from "./AvailableMeals.module.css";
-import { useCallback, useEffect, useState } from "react";
-
-// const DUMMY_MEALS = [
-//   {
-//     id: "m1",
-//     name: "Sushi",
-//     description: "Finest fish and veggies",
-//     price: 22.99,
-//   },
-//   {
-//     id: "m2",
-//     name: "Schnitzel",
-//     description: "A german specialty!",
-//     price: 16.5,
-//   },
-//   {
-//     id: "m3",
-//     name: "Barbecue Burger",
-//     description: "American, raw, meaty",
-//     price: 12.99,
-//   },
-//   {
-//     id: "m4",
-//     name: "Green Bowl",
-//     description: "Healthy...and green...",
-//     price: 18.99,
-//   },
-// ];
+import { useEffect, useState } from "react";
 
 const AvailableMeals = () => {
-  const [mealList, setMealList] = useState([]);
+  const [meals, setMeals] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setHttpError] = useState();
 
-  const fetchMeals = useCallback(async () => {
-    try {
+  useEffect(() => {
+    const fetchMeals = async () => {
       const res = await fetch(
         "https://react-2417f-default-rtdb.asia-southeast1.firebasedatabase.app/meals.json"
       );
-      if (!res.ok) throw new Error("Unable to connect to database");
+      if (!res.ok) {
+        throw new Error("Unable to connect to database");
+      }
       const data = await res.json();
       const loadedMeals = [];
 
@@ -52,17 +29,25 @@ const AvailableMeals = () => {
         });
       }
 
-      setMealList(loadedMeals);
-    } catch (err) {
-      console.error(err);
-    }
+      setMeals(loadedMeals);
+      setIsLoading(false);
+    };
+
+    fetchMeals().catch((err) => {
+      setHttpError(err.message);
+      setIsLoading(false);
+    });
   }, []);
 
-  useEffect(() => {
-    fetchMeals();
-  }, [fetchMeals]);
+  if (isLoading) {
+    return <p className={classes.MealsLoading}>Loading...</p>;
+  }
 
-  const mealsList = mealList.map((meal) => (
+  if (error) {
+    return <p className={classes.MealsError}>{error}</p>;
+  }
+
+  const mealsList = meals.map((meal) => (
     <MealItem
       key={meal.id}
       id={meal.id}
